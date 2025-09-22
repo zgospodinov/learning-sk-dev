2e86d374e225dabd775f1f1cd3599dd445107d63
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-09-22 16:13:12 +0300

    Add project files.

---
+++ sk-dev/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace sk_dev;
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var modelId = "gpt-4o";
+        var apiKey = Environment.GetEnvironmentVariable("OAI_SK_DEV_KEY") ?? throw new InvalidOperationException("Please set the OAI_SK_DEV_KEY environment variable.");
+
+
+        var builder = Kernel.CreateBuilder();
+        builder.AddOpenAIChatCompletion(
+            modelId: modelId,
+            apiKey: apiKey);
+
+        var kernel = builder.Build();
+        var chatHistory = new ChatHistory();
+        var chatCompletion = kernel.GetRequiredService<IChatCompletionService>();
+
+
+        while (true)
+        {
+            Console.WriteLine("\nEnter your prompt:");
+            var prompt = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(prompt)) break;
+
+            chatHistory.AddUserMessage(prompt);
+
+            var fullMessageResponse = string.Empty;
+            await foreach (var message in chatCompletion.GetStreamingChatMessageContentsAsync(chatHistory, PromptSettings.OpenAIPromptSettings))
+            {
+                Console.Write(message.Content);
+                fullMessageResponse += message.Content;
+            }
+
+            chatHistory.AddSystemMessage(fullMessageResponse);
+
+        }
+
+    }
+}
---
+++ sk-dev/PromptSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace sk_dev;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+internal class PromptSettings
+{
+    internal static OpenAIPromptExecutionSettings OpenAIPromptSettings = new()
+    {
+        ChatSystemPrompt = "You are a helpful AI assistant.",
+        Temperature = 0.2,
+        //MaxTokens = 800,
+        //TopP = 0.8,
+        //FrequencyPenalty = 0,
+        //PresencePenalty = 0,
+        //StopSequences = new List<string> { "###" }
+    };
+}
+
